bd917808f7136987470ecfffee5785a08b2913fe
Carlos Azpurua <carlosazpurua1@gmail.com>
2023-06-11 03:03:19 -0300

    feat: WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,13 @@
   import Home from '$lib/views/Home.svelte';
   import NotFound from '$lib/views/NotFound.svelte';
   import Question from '$lib/views/Create/Question.svelte';
+  import Quiz from '$lib/views/Create/Quiz.svelte';
 
   const routes = {
     '/': Home,
     '/create': Create,
     '/create/question': Question,
+    '/create/quiz': Quiz,
     // Catch-all, must be last
     '*': NotFound
   };
