b31b878e186039b7170950246c18cd29436fd9e6
Carlos Azpurua <carlosazpurua1@gmail.com>
2023-06-11 01:15:55 -0300

    feat: shuffle & functional questions (#14)
    <!--
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I
        have the right to submit it under the open source license
        indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best
        of my knowledge, is covered under an appropriate open source
        license and I have the right under that license to submit that
        work with modifications, whether created in whole or in part
        by me, under the same open source license (unless I am
        permitted to submit under a different license), as indicated
        in the file; or
    
    (c) The contribution was provided directly to me by some other
        person who certified (a), (b) or (c) and I have not modified
        it.
    
    (d) I understand and agree that this project and the contribution
        are public and that a record of the contribution (including all
        personal information I submit with it, including my sign-off) is
        maintained indefinitely and may be redistributed consistent with
        this project or the open source license(s) involved.
    -->

--- src/lib/views/Create/Question.svelte
+++ src/lib/views/Create/Question.svelte
@@ -1,32 +1,72 @@
 <script lang="ts">
   import Card from '$lib/components/Card.svelte';
   import View from '$lib/components/View.svelte';
+
+  let question = '';
+  let questionNumb = 0;
+  let questionEntries = JSON.parse(localStorage.getItem('Questions') as string);
+
+  if (questionEntries == null) questionEntries = [];
+  questionNumb = questionEntries.length + 1;
+
+  function handleSubmit() {
+    questionEntries.push(question);
+    localStorage.setItem('Questions', JSON.stringify(questionEntries));
+    questionNumb = questionNumb + 1;
+    question = '';
+  }
+
+  // Shuffle -- Algorithm Fisher-Yates
+  function shuffle(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function startQuiz() {
+    shuffle(questionEntries);
+    localStorage.setItem('Questions', JSON.stringify(questionEntries));
+  }
 </script>
 
 <View>
   <Card>
-    <div class="text-xl font-extrabold text-left pt-10">
-      <input
-        type="text"
-        class="mt-5 py-5 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-        name="Question"
-        placeholder="Agregar Pregunta"
-      />
-    </div>
+    <form on:submit|preventDefault={handleSubmit}>
+      <div class="flex text-xl font-extrabold text-left pt-10">
+        <p class="text-gray-700 mt-5 py-5 mr-5">
+          {questionNumb}.
+        </p>
+        <input
+          type="text"
+          class="mt-5 py-5 rounded-lg border-transparent appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          name="Question"
+          placeholder="Agregar Pregunta"
+          bind:value={question}
+        />
+      </div>
 
-    <div class="pt-20">
-      <button
-        type="button"
-        class="w-full max-w-2/3 py-4 px-4 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-lg px-5 text-center mr-2 mb-2"
-      >
-        Agregar Pregunta
-      </button>
-      <button
-        type="button"
-        class="w-full max-w-2/3 py-4 px-4 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-lg px-5 text-center mr-2 mb-2"
-      >
-        Completado
-      </button>
-    </div>
+      <div class="pt-20">
+        <button
+          type="submit"
+          class="w-full max-w-2/3 py-4 px-4 text-white
+        {!!question
+            ? 'bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600'
+            : 'bg-gradient-to-r from-pink-100 via-pink-200 to-pink-300'}
+        hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300
+        dark:focus:ring-pink-800 font-medium rounded-lg text-lg px-5 text-center mr-2 mb-2"
+          disabled={!question}
+        >
+          Agregar Pregunta
+        </button>
+        <button
+          type="button"
+          class="w-full max-w-2/3 py-4 px-4 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-lg px-5 text-center mr-2 mb-2"
+          on:click={startQuiz}
+        >
+          Completado
+        </button>
+      </div>
+    </form>
   </Card>
 </View>
